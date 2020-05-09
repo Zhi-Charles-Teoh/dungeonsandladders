029e847d35c69a8f8c73a18146ed2e55d4a53574
Zhi Charles Teoh <teohz2@student.unimelb.edu.au>
2020-05-09 22:49:56 +1000

    new update

--- models/userreg.js
+++
@@ -1,9 +0,0 @@
-function userRegForm() {
-  var x = document.forms["userRego"]["testinput"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-
-module.exports = userRegForm();
