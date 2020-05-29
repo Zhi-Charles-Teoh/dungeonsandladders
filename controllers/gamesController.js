83b1cf5288fe076e806b7b24082cd4dfa6351aec
Zhi Charles Teoh <teohz2@student.unimelb.edu.au>
2020-05-30 05:55:51 +1000

    altered controllers

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -21,3 +21,53 @@ const getGamesByTags = (req, res) => {
 module.exports = {
 	getAllGames, getGameByID, getGamesByTags
 };
+const mongoose = require("mongoose");
+
+// import author model
+const Game = mongoose.model("game");
+
+    
+// function to handle a request to get all authors
+const getAllGames = async (req, res) => {
+	try {
+		const all_games = await Game.find();
+		return res.send(all_games);
+	} catch (err) {
+		res.status(400);
+		return res.send("Database query failed");
+	}
+};
+ 
+
+// function to modify author by ID
+const updateGame = async (req, res) => {
+	res.send("Working on this feature");
+};
+
+
+const addGame = async (req, res) => {
+	res.send("Working on this feature");
+};
+
+const getGameByID = (req, res) => {
+	try {
+		const ID_game = await Game.find(ID_game => ID_game.id === req.params.id);
+		return res.send(ID_game);
+	} catch (err) {
+		res.status(400);
+		return res.send("Database query failed");
+	}
+};
+
+const getGamesByTags = (req, res) => {
+	res.send("Working on this feature");
+};
+
+// remember to export the functions
+module.exports = {
+  getAllGames,
+  getGameByID,
+  addGame,
+  updateGame,
+  getGamesByTags,
+};
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,3 +14,44 @@ const getUserByID = (req, res) => {
 module.exports = {
 	getAllUsers, getUserByID
 };
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
+
+
+const getAllUsers = async (req, res) => {
+	try {
+		const all_users = await User.find();
+		return res.send(all_users);
+	} catch (err) {
+		res.status(400);
+		return res.send("Database query failed");
+	}
+};
+ 
+
+const updateUser = async (req, res) => {
+	res.send("Working on this feature");
+};
+
+
+const addUser = async (req, res) => {
+	res.send("Working on this feature");
+};
+
+const getUserByID = (req, res) => {
+	try {
+		const ID_user = await User.find(ID_user => ID_user.id === req.params.id);
+		return res.send(ID_user);
+	} catch (err) {
+		res.status(400);
+		return res.send("Database query failed");
+	}
+};
+
+
+module.exports = {
+  getAllUsers,
+  getUserByID,
+  addUser,
+  updateUser,
+};
