1c7d847f4dff97500ebe043d5c5e61dcb6bc61a9
Zhi Charles Teoh <teohz2@student.unimelb.edu.au>
2020-05-30 05:33:50 +1000

    small changes to matchmaking.js

--- models/matchmakings.js
+++ models/matchmakings.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const sessionSchema = new mongoose.Schema({
 	start_time:{ type: Date, required: true },
 	location:{ type: String, required: true },
-	game:{ type: ObjectId, required: true },
-	host:{ type: ObjectId, required: true },
+	game:{ type: String, required: true },
+	host:{ type: String, required: true },
 	max_playercount: Number,
 	players: Array,
 	rsvp_by:{ type: Date, required: true },
